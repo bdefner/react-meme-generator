079edf7a516c871c84ab279b98e1ec35045abed3
Beppino Defner <hallo@deona.at>
2022-09-14 20:52:43 +0200

    Build basic structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
 import './App.css';
+import { useEffect, useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default function App() {
+  const [templateImg, setTemplateImg] = useState(0);
+  const [ImgTemplates, setImgTemplates] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.memegen.link/templates')
+      .then((res) => res.json())
+      .then((res) => setImgTemplates(res))
+      .catch(() => alert('We could not find that template.'));
+  }, []);
+
+  const templateUrls = [];
+
+  for (let i = 0; i < ImgTemplates.length; i++) {
+    templateUrls[i] = ImgTemplates[i].blank;
+    console.log(ImgTemplates[i].blank);
+  }
+  console.log(templateUrls[0]);
+
+  const templateImages = templateUrls.map((url, index) => (
+    <div className="template-img-wrap">
+      <img
+        className="template-img"
+        src={templateUrls[index]}
+        onClick={() => {
+          setTemplateImg(templateImages[index]);
+        }}
+      />
     </div>
+  ));
+
+  return (
+    <container className="app-wrap">
+      <div className="column">
+        <h2>1. scroll and select</h2>
+        <div className="template-selector-wrap">
+          {/*  */}
+          <div className="template-selector">{templateImages}</div>
+        </div>
+      </div>
+      <div className="column">
+        <h2>2. meme it up</h2>
+        <select
+          name="templates"
+          id="templates"
+          value={templateImg}
+          onChange={(e) => setTemplateImg(e.currentTarget.value)}
+        >
+          {ImgTemplates.map((item) => (
+            <option key={item.id} value={item.id}>
+              {item.id}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      <div className="column">
+        <h2>3. ready to share</h2>
+      </div>
+
+      <div></div>
+    </container>
   );
 }
-
-export default App;
