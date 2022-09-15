35cde8d5ca868a26df29d60ef1d517dc9779b8cf
Beppino Defner <hallo@deona.at>
2022-09-15 15:36:42 +0200

    Add wrapper for created meme

--- src/App.js
+++ src/App.js
@@ -3,24 +3,28 @@ import { useEffect, useState } from 'react';
 
 export default function App() {
   const [templateImg, setTemplateImg] = useState(0);
-  const [ImgTemplates, setImgTemplates] = useState([]);
+  const [imgTemplates, setImgTemplates] = useState([]);
+  const [topTextInputValue, setTopTextInputValue] = useState('');
+  const [bottomTextInputValue, setBottomTextInputValue] = useState('');
+  const [topText, setTopText] = useState('Free memes');
+  const [bottomText, setBottomText] = useState('for everyone');
 
   useEffect(() => {
     fetch('https://api.memegen.link/templates')
       .then((res) => res.json())
       .then((res) => setImgTemplates(res))
-      .catch(() => alert('We could not find that template.'));
+      .catch((error) => alert(error));
   }, []);
 
-  const templateImages = ImgTemplates.map((url, index) => (
-    <div key={'imageWrap:' + { index }} className="template-img-wrap">
+  const templateImages = imgTemplates.map((url, index) => (
+    <div key={`imageWrap of url: + ${url}`} className="template-img-wrap">
       <img
-        key={'templateImage: ' + { index }}
+        key={`templateImage of url:  + ${url}`}
         alt="meme-template"
         className="template-img"
-        src={ImgTemplates[index].blank}
+        src={imgTemplates[index].blank}
         onClick={() => {
-          setTemplateImg(ImgTemplates[index].id);
+          setTemplateImg(imgTemplates[index].id);
           console.log(templateImg);
         }}
         aria-hidden="true"
@@ -40,31 +44,69 @@ export default function App() {
       <div className="column">
         <div className="options-wrap">
           <h2>2. meme it up</h2>
-          <select
-            name="templates"
-            id="templates"
-            value={templateImg}
-            onChange={(element) => setTemplateImg(element.currentTarget.value)}
+          <form
+            onSubmit={(event) => {
+              event.preventDefault();
+            }}
           >
-            {ImgTemplates.map((item) => (
-              <option key={item.id} value={item.id}>
-                {item.id}
-              </option>
-            ))}
-          </select>
+            <select
+              name="templates"
+              id="template-selector"
+              value={templateImg}
+              onChange={(event) => setTemplateImg(event.currentTarget.value)}
+            >
+              {imgTemplates.map((item) => (
+                <option key={item.id} value={item.id}>
+                  {item.id}
+                </option>
+              ))}
+            </select>
+            <br />
+            <label
+              htmlFor="top-text-input"
+              onChange={() => {
+                setTopTextInputValue('Testify');
+                console.log('topTextValue: ', topTextInputValue);
+              }}
+            >
+              Top text
+            </label>
+            <input
+              id="top-text-input"
+              onChange={(event) => {
+                setTopTextInputValue(event.currentTarget.value);
+              }}
+            />
+            <br />
+            <label htmlFor="bottom-text-input">Bottom text</label>
+            <input
+              id="bottom-text-input"
+              onChange={(event) =>
+                setBottomTextInputValue(event.currentTarget.value)
+              }
+            />
+            <br />
+            <button
+              onClick={() => {
+                setTopText(topTextInputValue);
+                setBottomText(bottomTextInputValue);
+              }}
+            >
+              Generate
+            </button>
+          </form>
           <br />
-          <label htmlFor="top-text-input">Top text</label>
-          <input id="top-text-input" />
           <br />
-          <label htmlFor="bottom-text-input">Bottom text</label>
-          <input id="bottom-text-input" />
-          <br />
-          <button>Generate</button>
+          <p>topText: {topText}</p>
+          <p>bottomText: {bottomText}</p>
         </div>
       </div>
 
       <div className="column">
         <h2>3. ready to share</h2>
+        <div className="result wrap">
+          <img src="logo192.png" alt="created meme" />
+        </div>
       </div>
 
       <div></div>
